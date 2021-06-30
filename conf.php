705cbf3ce7f502eb1558d9bdc25f65b693ae523f
Alphataurus0 <85222893+Alphataurus0@users.noreply.github.com>
2021-06-30 23:40:10 +0300

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Alpha Taurus .
+ * User: Taurus
+ * Date: 30.06.2021
+ * Time: 15:38
+ */
+
+// insert your fancy crypto currency name here
+$crypto_name = 'crytpo';
+
+// enter root domain here ending with /
+$root = 'http://localhost/';
+
+// establish connection to postgres database
+$db_conn = pg_connect("host=localhost dbname= user= password=");
+
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by alphataurus.
+ * User: Taurus
+ * Date: 30.06.2021
+ * Time: 18:50
+ */
+
+include 'conf.php';
+
+/*
+    to do
+    - welcome screen text
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#343a40">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#343a40">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <title><?php echo($crypto_name); ?> wallet</title>
+
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include 'header.php'; ?>
+
+<main class="container">
+
+    <h3 class="mt-5 text-center"><?php echo($crypto_name); ?> welcome page</h3>
+
+
+
+</main>
+
+<?php include 'footer.php'; ?>
+
+<script src="jquery-3.3.1.js"></script>
+<script src="popper.min.js"></script>
+<script src="bootstrap.min.js"></script>
+</body>
+</html>
